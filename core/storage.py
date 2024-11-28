d7e356e4584a532ed46a766e7e9ba5b77b0a51a0
Rafnix Guzmán <rafnixg@gmail.com>
2024-11-28 11:23:25 -0500

    Add docstring to create_user_storage function for improved clarity and documentation

--- core/storage.py
+++ core/storage.py
@@ -1,9 +1,21 @@
+"""Storage utilities."""
+
 import os
 import uuid
 from flask import current_app
 
+
 def create_user_storage(storage_user_id):
-    storage_path = os.path.join(current_app.config['UPLOAD_FOLDER'], f"user_{uuid.uuid4().hex}_{storage_user_id}")
+    """Create a storage directory for a user
+    Args:
+        storage_user_id (int): User ID
+    Returns:
+        str: Path to the user's storage directory
+    """
+    storage_path = os.path.join(
+        current_app.config["UPLOAD_FOLDER"],
+        f"user_{uuid.uuid4().hex}_{storage_user_id}",
+    )
     if not os.path.exists(storage_path):
         os.makedirs(storage_path)
     return storage_path
