3fcc86d59f6c4263451004232b72c33d05e35f12
Rafnix Guzmán <rafnixg@gmail.com>
2024-11-28 11:22:38 -0500

    Add module docstring to extensions.py for clarity

--- app/extensions.py
+++ app/extensions.py
@@ -1,3 +1,5 @@
+"""Extensions module. Each extension is initialized in the app factory located in app/__init__.py."""
+
 from flask_sqlalchemy import SQLAlchemy
 from core.mail import Mail
 from flask_login import LoginManager
