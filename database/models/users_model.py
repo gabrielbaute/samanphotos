492f7ffed282e7e54baee69aafd63d00f122bbc4
gabrielbaute <gabrielbaute@gmail.com>
2025-03-26 16:20:48 -0400

    Campo username ya no es unique=true

--- database/models/users_model.py
+++ database/models/users_model.py
@@ -6,7 +6,7 @@ import uuid
 
 class User(UserMixin, db.Model):
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    username = db.Column(db.String(150), unique=True, nullable=False)
+    username = db.Column(db.String(150), unique=False, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='user')
