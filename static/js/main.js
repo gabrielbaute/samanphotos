ec51a17face48250688ac5820639dda2f9f80d64
gabrielbaute <gabrielbaute@gmail.com>
2025-03-26 12:57:49 -0400

    Agregados iconos de material icons

--- static/js/main.js
+++ static/js/main.js
@@ -17,13 +17,37 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Manejo de cierre de notificaciones flash
-    var deleteButtons = document.querySelectorAll('.notification .delete');
-    deleteButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            var notification = button.parentNode;
-            notification.parentNode.removeChild(notification);
+    const modal = document.getElementById("flash-modal");
+    const closeModalButtons = modal.querySelectorAll(".delete, .button.is-success");
+
+    // Abre automáticamente el modal si hay mensajes flash
+    const messages = modal.querySelectorAll(".message");
+    if (messages.length > 0) {
+        modal.classList.add("is-active");
+    }
+
+    // Cierra el modal al hacer clic en los botones de cierre
+    closeModalButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            modal.classList.remove("is-active");
         });
     });
+
+    // Constantes para detectar el boton de cambio de theme
+    const themeToggle = document.getElementById("theme-toggle");
+    const htmlElement = document.documentElement;
+
+    // Leer el tema inicial desde localStorage
+    const currentTheme = localStorage.getItem("theme") || "light";
+    htmlElement.setAttribute("data-theme", currentTheme);
+
+    // Función para actualizar el ícono del botón
+    const updateIcon = () => {
+        const activeTheme = htmlElement.getAttribute("data-theme");
+        themeToggle.innerHTML = activeTheme === "dark" 
+            ? '<i class="fa-solid fa-sun"></i>' 
+            : '<i class="fa-solid fa-moon"></i>';
+    };
 
     // Manejo de pestañas en la página de editar perfil
     const tabSecurity = document.getElementById('tab-security');
