5e2bfca9faf4b250aacac4e66c778b6e6f1bf4e4
gabrielbaute <gabrielbaute@gmail.com>
2025-03-26 09:22:58 -0400

    Reestructuración de la función de construcción

--- app/__init__.py
+++ app/__init__.py
@@ -1,72 +1,61 @@
 """Modulo de inicialización de la aplicación Flask"""
 
 import os
-import uuid
 
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
-from flask_security import Security, SQLAlchemyUserDatastore
-from flask_admin import Admin
+from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
-from flask_caching import Cache
-from flask_login import LoginManager, current_user
-from flask_jwt_extended import JWTManager
-from datetime import datetime
+from flask_login import current_user
 
-from app.extensions import mail, login_manager
-from database.models import User, Role
-from database.db_config import db
+from app.extensions import mail, login_manager, cache, jwt
+from database.models import User
+from database.db_config import db, init_db
 from app.routes import register_blueprints
 from core.samanapi import api_bp
 from utils import create_admin_user
+from config import Config
 
-cache=Cache()
-jwt=JWTManager()
+
 
 
 def create_app():
     """Crea una instancia de la aplicación Flask"""
-    app = Flask(__name__, static_url_path="/static")
-    app.config.from_object("config.Config")
+    app = Flask(__name__,
+                template_folder='../templates',
+                static_folder='../static')
+    
+    # Set config variables
+    app.config.from_object(Config)
+
+    # Initialize components
+    init_db(app)
+    mail.init_app(app)
+    migrate = Migrate(app, db)
+    login_manager.init_app(app)
+    login_manager.login_view = "main.login"
+    cache.init_app(app)
+    jwt.init_app(app)
+
+    # Blueprints register
+    register_blueprints(app)
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     with app.app_context():
-        db.init_app(app)
-        mail.init_app(app)
-        migrate = Migrate(app, db)
-        admin = Admin(app)
-        cache.init_app(app)
-        login_manager.init_app(app)
-        jwt.init_app(app)
-
-        login_manager.login_view = "main.login"
-
-        # Registrar rutas personalizadas
-        register_blueprints(app)
-
-        # Inicializar Flask-Security después de registrar rutas personalizadas
-        user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-        security = Security()
-        security.init_app(app, datastore=user_datastore, register_blueprint=False)
-
         db.create_all()
         create_admin_user()
-
-        @app.before_request
-        def update_last_login():
-            if current_user.is_authenticated:
-                current_user.last_login_at = current_user.current_login_at
-                current_user.last_login_ip = current_user.current_login_ip
-                current_user.current_login_at = datetime.utcnow()
-                current_user.current_login_ip = request.remote_addr
-                current_user.login_count += 1
-                db.session.commit()
 
     if not os.path.exists(app.config["UPLOAD_FOLDER"]):
         os.makedirs(app.config["UPLOAD_FOLDER"])
 
-    app.register_blueprint(api_bp, url_prefix='/api')
+    
+    
+    @app.context_processor
+    def inject_app_name():
+        return {
+            "app_name": app.config["APP_NAME"],
+            "app_version": app.config["APP_VERSION"],
+            "app_language": app.config["APP_LANGUAGE"]
+            }
     
     return app
 
