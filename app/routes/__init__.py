df3ffb0bfd987cd5a1e19168ce87a2ef451494cb
gabrielbaute <gabrielbaute@gmail.com>
2025-03-28 15:42:36 -0400

    Implementación de prefijos en los blueprints

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -8,7 +8,7 @@ from .public_routes import public
 
 def register_blueprints(app):
     """Register all blueprints in the app."""
-    app.register_blueprint(auth)
-    app.register_blueprint(edits)
-    app.register_blueprint(photos)
+    app.register_blueprint(auth, url_prefix="/auth")
+    app.register_blueprint(edits, url_prefix="/edit")
+    app.register_blueprint(photos, url_prefix="/photos")
     app.register_blueprint(public)
