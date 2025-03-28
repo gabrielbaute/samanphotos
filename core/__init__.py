fe3605ea603749a4e9a35c1459e9d3270a57dc5d
gabrielbaute <gabrielbaute@gmail.com>
2025-03-28 15:21:50 -0400

    Creación de almacenamiento para usuarios agregado al logger

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,10 @@
 from .facerecognition import process_photo, comparefaces
 from .metadata import extract_metadata
 from .storage import create_user_storage
+
+__all__ = [
+    "process_photo",
+    "comparefaces",
+    "extract_metadata",
+    "create_user_storage",
+]
