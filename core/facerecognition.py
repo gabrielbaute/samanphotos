1cbf48837780c84a9d9ab3f03f02e2d8d4b85b49
gabrielbaute <gabrielbaute@gmail.com>
2025-03-28 15:18:35 -0400

    Eventos de facerecognition registrados en el logger

--- core/facerecognition.py
+++ core/facerecognition.py
@@ -1,3 +1,4 @@
+from flask import current_app
 import face_recognition
 import numpy as np
 from database.models import Photo, FaceEncoding
@@ -5,56 +6,72 @@ from database.db_config import db
 
 def detect_faces(photo):
     """Detecta rostros en una foto y devuelve las codificaciones y ubicaciones"""
-    image=face_recognition.load_image_file(photo.path)
-    face_locations=face_recognition.face_locations(image)
-    face_encodings=face_recognition.face_encodings(image, face_locations)
-    return face_encodings, face_locations
+    try:
+        image=face_recognition.load_image_file(photo.path)
+        face_locations=face_recognition.face_locations(image)
+        face_encodings=face_recognition.face_encodings(image, face_locations)
+        return face_encodings, face_locations
+    except Exception as e:
+        current_app.logger.error(f"Error al procesar la imagen {photo.path}: {e}")
+        return [], []
 
 def process_encodings(photo, face_encodings, face_locations):
     """Verifica las coincidencias y guarda las nuevas codificaciones faciales"""
     try:
-        existing_encodings=FaceEncoding.query.all()
-        existing_encodings_list=[np.array(face.encoding) for face in existing_encodings]
+        try:
+            existing_encodings=FaceEncoding.query.all()
+            existing_encodings_list=[np.array(face.encoding) for face in existing_encodings]
+        except Exception as e:
+            current_app.logger.error(f"Error al obtener codificaciones existentes: {e}")
+            existing_encodings_list=[]
+
+        for encoding, location in zip(face_encodings, face_locations):
+            if existing_encodings_list:
+                matches=face_recognition.compare_faces(existing_encodings_list, encoding)
+            else:
+                matches=[]
+
+            if True not in matches:
+                top, right, bottom, left = location
+                face_encoding=FaceEncoding(
+                    encoding=encoding.tolist(),
+                    photo_id=photo.id,
+                    top=top,
+                    right=right,
+                    bottom=bottom,
+                    left=left
+                )
+                db.session.add(face_encoding)
+        db.session.commit()
     except Exception as e:
-        existing_encodings_list=[]
-
-    for encoding, location in zip(face_encodings, face_locations):
-        if existing_encodings_list:
-            matches=face_recognition.compare_faces(existing_encodings_list, encoding)
-        else:
-            matches=[]
-
-        if True not in matches:
-            top, right, bottom, left = location
-            face_encoding=FaceEncoding(
-                encoding=encoding.tolist(),
-                photo_id=photo.id,
-                top=top,
-                right=right,
-                bottom=bottom,
-                left=left
-            )
-            db.session.add(face_encoding)
-    db.session.commit()
+        current_app.logger.error(f"Error al procesar las codificaciones faciales: {e}")
 
 def process_photo(photo):
     """Procesa una foto completa"""
-    face_encodings, face_locations=detect_faces(photo)
-    if not face_encodings:
-        return # Si no se detectan codificaciones, no continuar
-    process_encodings(photo, face_encodings, face_locations)
+    try:
+        face_encodings, face_locations=detect_faces(photo)
+        if not face_encodings:
+            return # Si no se detectan codificaciones, no continuar
+        process_encodings(photo, face_encodings, face_locations)
+    except Exception as e:
+        current_app.logger.error(f"Error al procesar la foto {photo.path}: {e}")
+        raise e
 
 def comparefaces(target_encoding, tolerance=0.9):
     """Compara una codificación facial objetivo con todas las codificaciones faciales en la base de datos"""
-    all_faces = FaceEncoding.query.all()
-    print(f"Comparando con {len(all_faces)} codificaciones faciales en la base de datos.")
-    matches = []
-    for face in all_faces:
-        face_encoding_array = np.array(face.encoding)
-        print(f"Codificación objetivo: {target_encoding}")
-        print(f"Codificación del rostro {face.id}: {face_encoding_array}")
-        is_match = face_recognition.compare_faces([target_encoding], face_encoding_array, tolerance=tolerance)
-        print(f"Comparando con rostro {face.id}: {'Match' if True in is_match else 'No Match'}")
-        if True in is_match:
-            matches.append(face)
-    return matches
+    try:
+        all_faces = FaceEncoding.query.all()
+        print(f"Comparando con {len(all_faces)} codificaciones faciales en la base de datos.")
+        matches = []
+        for face in all_faces:
+            face_encoding_array = np.array(face.encoding)
+            print(f"Codificación objetivo: {target_encoding}")
+            print(f"Codificación del rostro {face.id}: {face_encoding_array}")
+            is_match = face_recognition.compare_faces([target_encoding], face_encoding_array, tolerance=tolerance)
+            print(f"Comparando con rostro {face.id}: {'Match' if True in is_match else 'No Match'}")
+            if True in is_match:
+                matches.append(face)
+        return matches
+    except Exception as e:
+        current_app.logger.error(f"Error al comparar rostros: {e}")
+        return []
