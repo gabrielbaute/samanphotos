c2c1066182990dd53407faa5944cbd9eab0d6119
gabrielbaute <gabrielbaute@gmail.com>
2025-03-28 10:38:20 -0400

    Agregada función de registro de auditorías

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,13 @@
 from .storage_id import generate_storage_user_id
 from .load_admin_user import create_admin_user
 from .config_log import setup_logging
+from .auditory_actions import ACCIONES
+from .auditory_register import registrar_auditoria
 
 __all__ = [
     'generate_storage_user_id',
     'create_admin_user',
-    'setup_logging'
+    'setup_logging',
+    'ACCIONES',
+    'registrar_auditoria'
     ]
---
+++ utils/auditory_actions.py
@@ -0,0 +1,11 @@
+ACCIONES = {
+    "CAMBIO_CONTRASENA": "Cambio de contraseña",
+    "CAMBIO_EMAIL": "Cambio de email principal",
+    "BLOQUEO_CUENTA": "Bloqueo de cuenta",
+    "ACTIVACION": "Nueva cuenta activada",
+    "REACTIVACION": "Solicitud de reactivación",
+    "HABILITAR_2FA": "Habilitación de 2FA",
+    "DESHABILITAR_2FA": "Deshabilitación de 2FA",
+    "ACTUALIZACION_PERFIL": "Actualización de datos del perfil",
+    "ELIMINACION_CUENTA": "Eliminación de cuenta",
+}
